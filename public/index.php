ef888f1bf0c3902f142d3330cb53c01ddd777449
Kuanysh <kuanyshmykyev@gmail.com>
2024-02-13 21:30:24 +0600

    routes.php

--- public/index.php
+++ public/index.php
@@ -6,13 +6,6 @@ use App\Router\Router;
 
 $router = new Router();
 
-$router->addRoute('GET', '/', 'views/home.php');
-$router->addRoute('GET', '/login', 'views/login.php');
-$router->addRoute('POST', '/login', 'views/login.php');
-$router->addRoute('GET', '/register', 'views/register.php');
-$router->addRoute('POST', '/register', 'views/register.php');
-
-
-
+require_once __DIR__ . "/../src/Router/routes.php";
 
 $router->dispatch($_SERVER['REQUEST_URI'], $_SERVER['REQUEST_METHOD']);
