81d5d91fb641acbde18a716886a7dd286af23ef7
kuanJoy <kuanyshmykyev@gmail.com>
2024-02-14 23:12:03 +0600

    added sql and navbar

--- public/views/layout/navbar.php
+++ public/views/layout/navbar.php
@@ -1,42 +1,37 @@
-<nav class="navbar navbar-expand-lg bg-body-tertiary">
-    <div class="container-fluid container">
-        <a class="navbar-brand" href="#"></a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Главная</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Парикмахеры</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Работы</a>
-                </li>
+<header class="p-3 mb-3 border-bottom">
+    <div class="container">
+        <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
+            <a href="/" class="d-flex align-items-center mb-2 mb-lg-0 text-dark text-decoration-none">
+                <svg class="bi me-2" width="40" height="32" role="img" aria-label="Bootstrap">
+                    <use xlink:href="#bootstrap"></use>
+                </svg>
+            </a>
+
+            <ul class="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
+                <li><a href="#" class="nav-link px-2 link-secondary">Big.Идея</a></li>
+                <li><a href="#" class="nav-link px-2 link-dark">Главная</a></li>
+                <li><a href="#" class="nav-link px-2 link-dark">Категории</a></li>
+                <li><a href="#" class="nav-link px-2 link-dark">Публикации</a></li>
             </ul>
-            <ul class="navbar-nav mr-auto mb-2 mb-lg-0">
-                <!-- <li class="nav-item dropdown">
-                    <a class="btn btn-primary" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                        Добавить
-                    </a>
-                    <ul class="dropdown-menu">
-                        <li><a class="dropdown-item" href="#">Работу</a></li>
-                        <li><a class="dropdown-item" href="#">Публикацию</a></li>
-                        <li>
-                            <hr class="dropdown-divider">
-                        </li>
-                        <li><a class="dropdown-item" href="#">Something else here</a></li>
-                    </ul>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Профиль</a>
+
+            <form class="col-12 col-lg-auto mb-3 mb-lg-0 me-lg-3">
+                <input type="search" class="form-control" placeholder="Search..." aria-label="Search">
+            </form>
+
+            <div class="dropdown text-end">
+                <a href="#" class="d-block link-dark text-decoration-none dropdown-toggle" id="dropdownUser1" data-bs-toggle="dropdown" aria-expanded="false">
+                    <img src="https://github.com/mdo.png" alt="mdo" width="32" height="32" class="rounded-circle">
+                </a>
+                <ul class="dropdown-menu text-small" aria-labelledby="dropdownUser1">
+                    <li><a class="dropdown-item" href="#">New project...</a></li>
+                    <li><a class="dropdown-item" href="#">Settings</a></li>
+                    <li><a class="dropdown-item" href="#">Profile</a></li>
+                    <li>
+                        <hr class="dropdown-divider">
                     </li>
-                </li> -->
-                <form class="d-flex" role="search">
-                    <button class="btn btn-dark" type="submit">Войти</button>
-                </form>
-            </ul>
+                    <li><a class="dropdown-item" href="#">Sign out</a></li>
+                </ul>
+            </div>
         </div>
     </div>
-</nav>
+</header>
---
+++ src/sql/sql.sql
@@ -0,0 +1,115 @@
+-- --------------------------------------------------------
+-- Хост:                         127.0.0.1
+-- Версия сервера:               5.6.51-log - MySQL Community Server (GPL)
+-- Операционная система:         Win64
+-- HeidiSQL Версия:              12.1.0.6537
+-- --------------------------------------------------------
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET NAMES utf8 */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+
+-- Дамп структуры базы данных edu
+CREATE DATABASE IF NOT EXISTS `edu` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci */;
+USE `edu`;
+
+-- Дамп структуры для таблица edu.categories
+CREATE TABLE IF NOT EXISTS `categories` (
+  `id` int(255) NOT NULL AUTO_INCREMENT,
+  `name` varchar(70) COLLATE utf8mb4_unicode_ci NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- Дамп данных таблицы edu.categories: ~0 rows (приблизительно)
+
+-- Дамп структуры для таблица edu.comments
+CREATE TABLE IF NOT EXISTS `comments` (
+  `id` int(255) NOT NULL AUTO_INCREMENT,
+  `post_id` int(255) DEFAULT '0',
+  `user_id` int(255) DEFAULT '0',
+  `content` int(255) DEFAULT '0',
+  `created_time` timestamp NULL DEFAULT '0000-00-00 00:00:00',
+  PRIMARY KEY (`id`) USING BTREE,
+  KEY `fk_comments_post_id` (`post_id`),
+  KEY `fk_comments_user_id` (`user_id`),
+  CONSTRAINT `fk_comments_post_id` FOREIGN KEY (`post_id`) REFERENCES `posts` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `fk_comments_user_id` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- Дамп данных таблицы edu.comments: ~0 rows (приблизительно)
+
+-- Дамп структуры для таблица edu.hashtags
+CREATE TABLE IF NOT EXISTS `hashtags` (
+  `id` int(250) NOT NULL AUTO_INCREMENT,
+  `name` int(70) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- Дамп данных таблицы edu.hashtags: ~0 rows (приблизительно)
+
+-- Дамп структуры для таблица edu.posts
+CREATE TABLE IF NOT EXISTS `posts` (
+  `id` int(255) NOT NULL AUTO_INCREMENT,
+  `title` varchar(70) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `content` text COLLATE utf8mb4_unicode_ci,
+  `status` enum('активен','скрыт') COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `category_id` int(255) DEFAULT NULL,
+  `user_id` int(255) DEFAULT NULL,
+  PRIMARY KEY (`id`) USING BTREE,
+  KEY `fk_posts_category_id` (`category_id`),
+  KEY `fk_posts_user_id` (`user_id`),
+  CONSTRAINT `fk_posts_category_id` FOREIGN KEY (`category_id`) REFERENCES `categories` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `fk_posts_user_id` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- Дамп данных таблицы edu.posts: ~0 rows (приблизительно)
+
+-- Дамп структуры для таблица edu.post_hashtags
+CREATE TABLE IF NOT EXISTS `post_hashtags` (
+  `post_id` int(255) DEFAULT NULL,
+  `hashtag_id` int(255) DEFAULT NULL,
+  KEY `fk_post_hashtags_post_id` (`post_id`),
+  KEY `fk_post_hashtags_hashtag_id` (`hashtag_id`),
+  CONSTRAINT `fk_post_hashtags_hashtag_id` FOREIGN KEY (`hashtag_id`) REFERENCES `hashtags` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `fk_post_hashtags_post_id` FOREIGN KEY (`post_id`) REFERENCES `posts` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- Дамп данных таблицы edu.post_hashtags: ~0 rows (приблизительно)
+
+-- Дамп структуры для таблица edu.post_likes
+CREATE TABLE IF NOT EXISTS `post_likes` (
+  `id` int(255) NOT NULL AUTO_INCREMENT,
+  `post_id` int(255) DEFAULT '0',
+  `user_id` int(255) DEFAULT '0',
+  PRIMARY KEY (`id`) USING BTREE,
+  KEY `fk_post_likes_post_id` (`post_id`),
+  KEY `fk_post_likes_user_id` (`user_id`),
+  CONSTRAINT `fk_post_likes_post_id` FOREIGN KEY (`post_id`) REFERENCES `posts` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `fk_post_likes_user_id` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- Дамп данных таблицы edu.post_likes: ~0 rows (приблизительно)
+
+-- Дамп структуры для таблица edu.users
+CREATE TABLE IF NOT EXISTS `users` (
+  `id` int(255) NOT NULL AUTO_INCREMENT,
+  `username` varchar(50) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `email` varchar(70) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `password` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `unique_email` (`email`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- Дамп данных таблицы edu.users: ~0 rows (приблизительно)
+
+/*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IFNULL(@OLD_FOREIGN_KEY_CHECKS, 1) */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40111 SET SQL_NOTES=IFNULL(@OLD_SQL_NOTES, 1) */;
