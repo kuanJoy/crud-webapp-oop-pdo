f986108b36d8a795ec9741fcdf2135f600e245b3
kuanJoy <kuanyshmykyev@gmail.com>
2024-02-14 10:52:26 +0600

    fixes

--- public/index.php
+++ public/index.php
@@ -1,5 +1,5 @@
 <?php
-require_once __DIR__ . '/../vendor/autoload.php';
+require_once __DIR__ . "/../vendor/autoload.php";
 require_once __DIR__ . "/../src/config/functions.php";
 
 use App\Router\Router;
