9366e87cbc6b157c445f3cfe03b79b6afafd6c15
kuanJoy <kuanyshmykyev@gmail.com>
2024-02-16 13:28:40 +0600

    import from oop

---
+++ src/Router/routes.php
@@ -0,0 +1,6 @@
+<?php
+$router->addRoute('GET', '/', 'views/home.php');
+$router->addRoute('GET', '/login', 'views/login.php');
+$router->addRoute('POST', '/login', 'views/login.php');
+$router->addRoute('GET', '/register', 'views/register.php');
+$router->addRoute('POST', '/register', 'views/register.php');
---
+++ src/config/config.php
@@ -0,0 +1,6 @@
+<?php
+require_once "rb.php";
+
+use RedBeanPHP;
+
+R::setup('mysql:host=localhost;dbname=shop', 'root', '');
