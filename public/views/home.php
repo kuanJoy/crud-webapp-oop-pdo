67b4eae43dbf1e9889154ef5fa0d8d62a6a541c4
Kuanysh <kuanyshmykyev@gmail.com>
2024-02-17 18:46:38 +0600

    90% verstky bannera and hashtag

--- public/views/home.php
+++ public/views/home.php
@@ -2,4 +2,5 @@
 include __DIR__ . "/layout/header.php";
 include __DIR__ . "/layout/navbar.php";
 include __DIR__ . "/include/banner.php";
+include __DIR__ . "/include/hashtags.php";
 include __DIR__ . "/layout/footer.php";
