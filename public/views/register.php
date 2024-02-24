fd547521f797200444ad61273d69e452ff4a3fed
Kuanysh <kuanyshmykyev@gmail.com>
2024-02-24 18:20:29 +0600

    fix

--- public/views/register.php
+++ public/views/register.php
@@ -1,11 +1,14 @@
 <?php
-include __DIR__ . "/layout/header.php";
-include __DIR__ . "/layout/navbar.php";
 
 use App\Controllers\AuthController;
+
+// require_once __DIR__ . '/../../src/app/controllers/AuthController.php';
+// require_once __DIR__ . '/../../src/app/models/AuthModel.php';
 
 $authController = new AuthController();
 $authController->regUser();
 
+include __DIR__ . "/layout/header.php";
+include __DIR__ . "/layout/navbar.php";
 include __DIR__ . "/include/register-form.php";
 include __DIR__ . "/layout/footer.php";
