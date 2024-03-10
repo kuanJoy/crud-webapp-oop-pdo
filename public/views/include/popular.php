5bbecd8e43b1db8614aab3b028b9697d1a8f580c
Kuanysh <kuanyshmykyev@gmail.com>
2024-03-10 19:38:16 +0600

    popular page fixes

--- public/views/include/popular.php
+++ public/views/include/popular.php
@@ -5,10 +5,10 @@
                 <h3 class="category__name">
                     <svg class="icon i-category">
                         <use href="/public/assets/images/svg/sprites.svg#folder"></use>
-                    </svg> <?= $category['category_name'] ?>
+                    </svg> <?= $category['category_name'] ?> - <b><?= $category['post_count'] ?></b>
                 </h3>
-                <a href="/category">
-                    Смотреть все <b><?= $category['post_count'] ?></b>
+                <a href="/category/<?= $category['category_id'] ?>">
+                    Смотреть все
                     <svg class="icon i-category">
                         <use href="/public/assets/images/svg/sprites.svg#more" />
                     </svg></a>
