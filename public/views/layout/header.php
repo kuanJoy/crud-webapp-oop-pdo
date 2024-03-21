b08b7123715427f90290cb03987c31a4f210e7e3
Kuanysh <kuanyshmykyev@gmail.com>
2024-03-21 11:25:02 +0600

    tinymce full version

--- public/views/layout/header.php
+++ public/views/layout/header.php
@@ -5,13 +5,6 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <script src="https://cdn.tiny.cloud/1/pkp4f0mwyp4n29afsvi1lveanjisusggn9zp4i5nhgsbdtkb/tinymce/6/tinymce.min.js" referrerpolicy="origin"></script>
-    <script>
-        tinymce.init({
-            selector: '#mytextarea',
-            plugins: 'image link',
-            toolbar: 'image link',
-        });
-    </script>
     <link rel="shortcut icon" href="../assets/images/svg/idea.svg" type="image/svg" />
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css" />
     <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
