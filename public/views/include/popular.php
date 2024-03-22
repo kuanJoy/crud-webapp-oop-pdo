0dfa71c314a6e19793106a38b13696d5735b3fba
Kuanysh <kuanyshmykyev@gmail.com>
2024-03-22 20:06:35 +0600

    new top system

--- public/views/include/popular.php
+++ public/views/include/popular.php
@@ -38,6 +38,7 @@
 
 <h3 class="category__name" style="margin-top: 2rem">Топ авторов</h3>
 <div class="popular">
+    <?php var_dump($topUsers) ?>
     <?php foreach ($topUsers as $user) : ?>
         <div class="popular__category">
             <div class="category-name-more" style="justify-content: center;">
@@ -46,23 +47,29 @@
                         <?= $user['username'] ?>
                     </h3>
                     <div style="display: flex; align-items:center; justify-content: center">
-                        <svg class="icon i-category">
-                            <use href="/public/assets/images/svg/sprites.svg#like"></use>
-                        </svg><b><?= $user['total_likes'] ?></b>
+                        <img src="/public/assets/images/svg/idea.svg" class="footer__logo_pic"><b><?= $user['total_likes'] + $user['total_posts'] ?></b>
                     </div>
                 </div>
                 <div style="display: flex; align-items: center; gap: 0.5rem">
                     <a href="/favourite/<?= $user['id'] ?>" style="background-color: #e2e5f6; padding: 0.3125rem 0.625rem;
     border-radius: 0.5rem;">
                         избранное
-                        <svg class="icon i-category">
-                            <use href="/public/assets/images/svg/sprites.svg#favourite" />
-                        </svg></a>
+                        <div style="display: flex; align-items:center; justify-content: center">
+                            <svg class="icon i-category">
+                                <use href="/public/assets/images/svg/sprites.svg#favourite" />
+                            </svg>
+                            <b><?= $user['total_likes'] ?></b>
+                        </div>
+                    </a>
                     <a href="/user/<?= $user['id'] ?>" style="background-color: #e2e5f6; padding: 0.3125rem 0.625rem; border-radius: 0.5rem;">
                         публикации
-                        <svg class="icon i-category">
-                            <use href="/public/assets/images/svg/sprites.svg#posts" />
-                        </svg></a>
+                        <div style="display: flex; align-items:center; justify-content: center">
+                            <svg class="icon i-category">
+                                <use href="/public/assets/images/svg/sprites.svg#posts" />
+                            </svg>
+                            <b><?= $user['total_posts'] ?></b>
+                        </div>
+                    </a>
                 </div>
             </div>
         </div>
