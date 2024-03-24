678b90ecab5f4e6cd728a423a4fd3e35a9c15e11
Kuanysh <kuanyshmykyev@gmail.com>
2024-03-25 01:47:10 +0600

    tinymce starter pack

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -19,22 +19,71 @@ hamburger.addEventListener("click", () => {
 });
 
 // TinyMC
+// tinymce.init({
+//   selector: "textarea",
+//   plugins:
+//     "anchor autolink charmap codesample emoticons image link lists media searchreplace table visualblocks wordcount checklist mediaembed casechange export formatpainter pageembed linkchecker a11ychecker permanentpen powerpaste advtable advcode editimage advtemplate ai mentions tinycomments tableofcontents footnotes mergetags autocorrect typography inlinecss",
+//   toolbar:
+//     "undo redo | blocks fontfamily fontsize | bold italic underline strikethrough | link image media table mergetags | addcomment showcomments | spellcheckdialog a11ycheck typography | align lineheight | checklist numlist bullist indent outdent | emoticons charmap | removeformat",
+//   tinycomments_mode: "embedded",
+//   tinycomments_author: "Author name",
+//   mergetags_list: [
+//     { value: "First.Name", title: "First Name" },
+//     { value: "Email", title: "Email" },
+//   ],
+//   ai_request: (request, respondWith) =>
+//     respondWith.string(() =>
+//       Promise.reject("See docs to implement AI Assistant")
+//     ),
+// });
+
 tinymce.init({
   selector: "textarea",
-  plugins:
-    "anchor autolink charmap codesample emoticons image link lists media searchreplace table visualblocks wordcount checklist mediaembed casechange export formatpainter pageembed linkchecker a11ychecker permanentpen powerpaste advtable advcode editimage advtemplate ai mentions tinycomments tableofcontents footnotes mergetags autocorrect typography inlinecss",
-  toolbar:
-    "undo redo | blocks fontfamily fontsize | bold italic underline strikethrough | link image media table mergetags | addcomment showcomments | spellcheckdialog a11ycheck typography | align lineheight | checklist numlist bullist indent outdent | emoticons charmap | removeformat",
-  tinycomments_mode: "embedded",
-  tinycomments_author: "Author name",
-  mergetags_list: [
-    { value: "First.Name", title: "First Name" },
-    { value: "Email", title: "Email" },
-  ],
-  ai_request: (request, respondWith) =>
-    respondWith.string(() =>
-      Promise.reject("See docs to implement AI Assistant")
-    ),
+  plugins: "anchor autolink charmap codesample emoticons image link lists media searchreplace table textcolor visualblocks wordcount linkchecker",
+  toolbar: "fontselect fontsizeselect undo redo | formatselect | bold italic underline strikethrough | link image media | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | emoticons charmap | removeformat | forecolor backcolor",
+  menubar: false,
+  fontsize_formats: '8pt 10pt 12pt 14pt 18pt 24pt 36pt',
+  textcolor_map: [
+    "000000", "Black",
+    "993300", "Burnt orange",
+    "333300", "Dark olive",
+    "003300", "Dark green",
+    "003366", "Dark azure",
+    "000080", "Navy Blue",
+    "333399", "Indigo",
+    "333333", "Very dark gray",
+    "800000", "Maroon",
+    "FF6600", "Orange",
+    "808000", "Olive",
+    "008000", "Green",
+    "008080", "Teal",
+    "0000FF", "Blue",
+    "666699", "Grayish blue",
+    "808080", "Gray",
+    "FF0000", "Red",
+    "FF9900", "Amber",
+    "99CC00", "Yellow green",
+    "339966", "Sea green",
+    "33CCCC", "Turquoise",
+    "3366FF", "Royal blue",
+    "800080", "Purple",
+    "999999", "Medium gray",
+    "FF00FF", "Magenta",
+    "FFCC00", "Gold",
+    "FFFF00", "Yellow",
+    "00FF00", "Lime",
+    "00FFFF", "Aqua",
+    "00CCFF", "Sky blue",
+    "993366", "Red violet",
+    "FFFFFF", "White",
+    "FF99CC", "Pink",
+    "FFCC99", "Peach",
+    "FFFF99", "Light yellow",
+    "CCFFCC", "Pale green",
+    "CCFFFF", "Pale cyan",
+    "99CCFF", "Light sky blue",
+    "CC99FF", "Plum"
+  ]
 });
 
 // ADD INPUTS FOR HASHTAG
